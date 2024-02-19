bbd492f197bc294aad78afd1f3abf91c436fe85d
tetiana <tchirska@gmail.com>
2024-02-19 13:31:42 +0200

    delete dist

--- dist/router/weatherRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.weatherRouter = void 0;
-const express_1 = require("express");
-const weatherControlers_1 = require("../controlers/weatherControlers");
-const router = (0, express_1.Router)();
-router.get("/", weatherControlers_1.weatherControllers.getByCity);
-router.get('/cord', weatherControlers_1.weatherControllers.getByCord);
-exports.weatherRouter = router;
