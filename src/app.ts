270d67156ca6aa3db2c7b71770993c39cea5f614
tetiana <tchirska@gmail.com>
2024-02-26 14:36:57 +0200

    add refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,102 +1,20 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
-import * as path from "path";
 
 
 import { weatherRouter } from './router/weatherRouter';
 import { configs } from './configs/config';
+import swaggerConfig from "./configs/swaggerConfig";
 
 const app = express();
 
 app.use('/weather', weatherRouter);
 
 
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Weather API Documentation',
-            version: '1.0.0',
-            description: 'API documentation for weather endpoints',
-        },
-        components: {
-            schemas: {
-                WeatherResponse: {
-                    type: 'object',
-                    properties: {
-                        coord: {
-                            type: 'object',
-                            properties: {
-                                lon: { type: 'number' },
-                                lat: { type: 'number' }
-                            }
-                        },
-                        weather: {
-                            type: 'array',
-                            items: {
-                                type: 'object',
-                                properties: {
-                                    id: { type: 'number' },
-                                    main: { type: 'string' },
-                                    description: { type: 'string' },
-                                    icon: { type: 'string' }
-                                }
-                            }
-                        },
-                        base: { type: 'string' },
-                        main: {
-                            type: 'object',
-                            properties: {
-                                temp: { type: 'number' },
-                                feels_like: { type: 'number' },
-                                temp_min: { type: 'number' },
-                                temp_max: { type: 'number' },
-                                pressure: { type: 'number' },
-                                humidity: { type: 'number' }
-                            }
-                        },
-                        visibility: { type: 'number' },
-                        wind: {
-                            type: 'object',
-                            properties: {
-                                speed: { type: 'number' },
-                                deg: { type: 'number' }
-                            }
-                        },
-                        clouds: {
-                            type: 'object',
-                            properties: {
-                                all: { type: 'number' }
-                            }
-                        },
-                        dt: { type: 'number' },
-                        sys: {
-                            type: 'object',
-                            properties: {
-                                type: { type: 'number' },
-                                id: { type: 'number' },
-                                country: { type: 'string' },
-                                sunrise: { type: 'number' },
-                                sunset: { type: 'number' }
-                            }
-                        },
-                        timezone: { type: 'number' },
-                        id: { type: 'number' },
-                        name: { type: 'string' },
-                        cod: { type: 'number' }
-                    }
-                }
 
-            }
-        }
-    },
-
-    apis: [path.resolve(__dirname, './router/*.ts')],
-};
-const specs = swaggerJsdoc(options);
+const specs = swaggerJsdoc(swaggerConfig);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
 
 
 const PORT = configs.PORT ;
